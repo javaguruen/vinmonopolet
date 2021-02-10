dc23ef5e1341fc829c644a3e70c9c56ed3122d73
Bjørn Hamre <javaguruen@gmail.com>
2021-02-10 20:47:28 +0100

    Whisky detail

--- frontend/src/views/WhiskyDetails.vue
+++ frontend/src/views/WhiskyDetails.vue
@@ -5,73 +5,82 @@
         <h2>{{this.whisky.varenavn}}</h2>
       </div>
     </div>
+
     <div class='row'>
-      <div class="col-2">Varenummer: </div>
-      <div class="col-1">{{this.whisky.varenummer}}</div>
-      <div class="col-2">Navn: </div>
-      <div class="col-7">{{this.whisky.varenavn}}</div>
-    </div>
-    <div class='row'>
-      <div class="col-1">{{this.whisky.alkohol}}%</div>
-      <div class="col-1">{{this.whisky.volum}}l</div>
-      <div class="col-1">Pris: </div>
-      <div class="col-1">{{this.whisky.prices[0].pris}},-</div>
-      <div class="col-1">Literpris: </div>
-      <div class="col-1">{{this.whisky.prices[0].literpris }},-</div>
-      <div class="col-2">{{ this.aargang() }} </div>
+      <div class="col-6 text-right header">Varenummer: </div>
+      <div class="col-6 text-left">{{this.whisky.varenummer}}</div>
     </div>
     <div class='row'>
-      <div class="col-1">Land: </div>
-      <div class="col-1">{{this.whisky.land}}</div>
-      <div class="col-2">Distrikt: </div>
-      <div class="col-1">{{this.whisky.distrikt}}</div>
-      <div class="col-2">Underdistrikt: </div>
-      <div class="col-1">{{this.whisky.underdistrikt}}</div>
+      <div class="col-6 text-right header">Destilleri: </div>
+      <div class="col-6 text-left">{{this.whisky.produsent}}</div>
     </div>
-    <div class="row">
-      <div class="col-1">produsent:</div>
-      <div class="col-5">{{ this.whisky.produsent}}</div>
-      <div class="col-1">Grossist: </div>
-      <div class="col-5">{{ this.whisky.grossist}}</div>
+    <div v-if='this.whisky.aargang !== 0 '>
+      <div class='row'>
+        <div class="col-6 text-right header">Årgang: </div>
+        <div class="col-6 text-left">{{this.whisky.aargang}}</div>
+      </div>
     </div>
-    <div class="row">
-      <div class="col-2 text-left">Farge:</div>
-      <div class="col-10 text-left">{{ this.whisky.farge}}</div>
+    <div class='row'>
+      <div class="col-6 text-right header">Styrke: </div>
+      <div class="col-6 text-left">{{this.whisky.alkohol}}%</div>
     </div>
-    <div class="row">
-      <div class="col-2 text-left">Lukt:</div>
-      <div class="col-10 text-left">{{ this.whisky.lukt}}</div>
-    </div>
-    <div class="row">
-      <div class="col-2 text-left">Smak:</div>
-      <div class="col-10 text-left">{{ this.whisky.smak}}</div>
+    <div class='row'>
+      <div class="col-6 text-right header">Pris: </div>
+      <div class="col-6 text-left keep-spaces">{{ formatterPris (this.whisky.prices[0].pris )}}</div>
     </div>
 
+    <div class='row'>
+      <div class="col-6 text-right header">Land: </div>
+      <div class="col-6 text-left">{{this.whisky.land}}</div>
+    </div>
+    <div class='row'>
+      <div class="col-6 text-right header">Distrikt: </div>
+      <div class="col-6 text-left">{{this.whisky.distrikt}}</div>
+    </div>
+    <div class='row'>
+      <div class="col-6 text-right header">Underdistrikt: </div>
+      <div class="col-6 text-left">{{this.whisky.underdistrikt}}</div>
+    </div>
+
+    <div class='row'>
+      <div class="col-6 text-right header">Farge: </div>
+      <div class="col-6 text-left">{{this.whisky.farge}}</div>
+    </div>
+    <div class='row'>
+      <div class="col-6 text-right header">Lukt: </div>
+      <div class="col-6 text-left">{{this.whisky.lukt}}</div>
+    </div>
+    <div class='row'>
+      <div class="col-6 text-right header">Smak: </div>
+      <div class="col-6 text-left">{{this.whisky.smak}}</div>
+    </div>
     <p></p>
     <div class='row'>
-      <div class="col-12 text-left">
-        <h3>Prishistorikk</h3>
-      </div>
+      <div class="col-12"><h3>Prishistorikk</h3></div>
     </div>
-
     <div class="row">
-      <div class="col-4"><b>Dato</b></div>
-      <div class="col-4"><b>Pris</b></div>
-      <div class="col-4"><b>Literpris</b></div>
+      <div class="col-12 col-sm-6 header">Dato</div>
+      <div class="col-12 col-sm-6 header">Pris (per/liter)</div>
     </div>
-    <div v-for="pris in this.whisky.prices">
-      <div class="row">
-        <div class="col-4">{{ pris.datotid}}</div>
-        <div class="col-4">{{ pris.pris}}</div>
-        <div class="col-4">{{ pris.literpris}}</div>
+    <div v-for="(pris, index) in this.whisky.prices" v-bind:key="pris.id">
+      <div class="row"  v-bind:class="[ index%2 === 0 ? 'row0' : 'row1']">
+        <div class="col-12 col-sm-6 index" >{{ formatterDatotid(pris.datotid) }}</div>
+        <div class="col-12 col-sm-6 keep-spaces">{{ formatterPris( pris.pris )}} ({{ formatterPris( pris.literpris )}})</div>
       </div>
     </div>
-
   </div>
 </template>
-<script>
 
+<style>
+.header { font-weight: bold; }
+.keep-spaces { white-space: pre-wrap; }
+.row0 { background-color: #f8f8f8; }
+.row1 { background-color: #eeeeee; }
+</style>
+
+<script>
 import axios from 'axios'
+import dateFormat from 'dateformat'
 
 export default {
   name: 'whiskydetails',
@@ -82,16 +91,17 @@ export default {
     }
   },
   mounted: function () {
-    console.log('mounted')
     this.findWhisky() // method1 will execute at pageload
   },
   methods: {
-    aargang () {
-      if (this.whisky.aargang === 0) {
-        return ''
-      } else {
-        return 'Årgang: ' + this.whisky.aargang
-      }
+    formatterDatotid (d) {
+      let millis = Date.parse(d)
+      let date = new Date(millis)
+      return dateFormat(date, 'dd.mm.yyyy HH:MM:ss')
+    },
+    formatterPris (pris) {
+      let formatted = ((Math.round(pris * 100) / 100).toFixed(2)).padStart(9)
+      return formatted
     },
     findWhisky () {
       console.log('findWhisky by id ' + this.$route.params.whiskyid)
